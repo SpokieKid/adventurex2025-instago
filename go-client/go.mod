f39de53568ffd393a7b702ed7faa8b251a718a77
qujing226 <qujing226@gmail.com>
2025-07-23 22:47:30 +0800

    new

--- go-client/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/adventurex2025-instago
-
-go 1.24.4
--- go-client/main.go
+++ go-client/main.go
@@ -1,20 +1,13 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	// 监听http9090端口，get方法，ping路由，返回json格式，key：message，value：pong
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		// 设置响应头为JSON格式
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(map[string]string{"message": "pong"})
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-
-	log.Println("服务器启动，监听 :9090 端口...")
-	log.Fatal(http.ListenAndServe(":9090", nil))
+	router.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
