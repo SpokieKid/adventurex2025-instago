e83c7cb59252516a124747ac44e01af6cebdcac9
Spokiekid <assassins010@outlook.com>
2025-07-24 12:49:51 +0800

    mac initial commit

---
+++ mac-client/InstaGoTests/InstaGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaGoTests.swift
+//  InstaGoTests
+//
+//  Created by 陈瀚翔 on 23/7/2025.
+//
+
+import Testing
+@testable import InstaGo
+
+struct InstaGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
